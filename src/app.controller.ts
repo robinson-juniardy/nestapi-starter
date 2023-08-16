c59ae7a9351d579e7240fa7d18e7ecae037e7513
robinson-juniardy <robinson.juniardy@gmail.com>
2023-08-16 23:19:48 +0800

    add auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @UseGuards(AuthGuard('local'))
+    @Post('auth/login')
+    async login(@Request() req){
+        return req.user
+    }
 }
